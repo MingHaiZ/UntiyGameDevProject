05cd20d1e5c15526f595edc57efa83d627614767
MingHaiZ <1163094232@qq.com>
2025-05-29 21:55:18 +0800

    <修改> 1.优化攻击手感,在进入状态时根据输入判断攻击反向,而不是使用当前朝向 攻击->结束->闲置->移动.转向->攻击 攻击->结束->判断当前输入.转向->攻击

--- Assets/PlayerPrimaryAttackState.cs
+++ Assets/PlayerPrimaryAttackState.cs
@@ -23,8 +23,19 @@ public class PlayerPrimaryAttackState : PlayerState
         }
 
         player.anim.SetInteger(PlayerStateConstants.PlayerComboCounter, comboCounter);
+
+        #region Choose attack direction
+
+        float attackDir = player.facingDir;
+        if (xInput != 0)
+        {
+            attackDir = xInput;
+        }
+
+        #endregion
+
         player.SetVelocity(
-            player.attackMovement[comboCounter].x * player.facingDir,
+            player.attackMovement[comboCounter].x * attackDir,
             player.attackMovement[comboCounter].y
         );
         stateTimer = 0.1f;
