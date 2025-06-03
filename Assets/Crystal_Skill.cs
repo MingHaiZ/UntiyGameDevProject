3d259cd15cb38391ffb071e07f461bbe5d65b373
MingHaiZ <1163094232@qq.com>
2025-06-03 14:22:56 +0800

    <修改> 1.使用List存储多个水晶并允许在使用后一段时间内自动补充 2.if (crystalList.Count == amountOfStacks)                 {                     Invoke(nameof(ResetAbility), useTimeWindow);                 } 这个放最前面检查是如果是满的时候触发,在第一发打出去之后就会安排这个invoke了

--- Assets/Crystal_Skill.cs
+++ Assets/Crystal_Skill.cs
@@ -16,9 +16,22 @@ public class Crystal_Skill : Skill
 
     [SerializeField] private float moveSpeed;
 
+    [Header("Multi stacking crystal")]
+    [SerializeField] private bool canUseMultiStacks;
+
+    [SerializeField] private int amountOfStacks;
+    [SerializeField] private float multiStackCooldown;
+    [SerializeField] private float useTimeWindow;
+    [SerializeField] private List<GameObject> crystalList = new List<GameObject>();
+
     public override void useSkill()
     {
         base.useSkill();
+
+        if (CanUseMultiCtystal())
+        {
+            return;
+        }
 
         if (currentCrystal == null)
         {
@@ -32,12 +45,65 @@ public class Crystal_Skill : Skill
             {
                 return;
             }
-            
+
             Vector2 playerPosition = player.transform.position;
             player.transform.position = currentCrystal.transform.position;
             currentCrystal.transform.position = playerPosition;
 
             currentCrystal.GetComponent<Crystal_Skill_Controller>()?.FinishCrystal();
         }
+    }
+
+    private bool CanUseMultiCtystal()
+    {
+        if (canUseMultiStacks)
+        {
+            if (crystalList.Count > 0)
+            {
+                if (crystalList.Count == amountOfStacks)
+                {
+                    Invoke(nameof(ResetAbility), useTimeWindow);
+                }
+
+                coolDown = 0;
+                GameObject crystalToRespawn = crystalList[crystalList.Count - 1];
+                GameObject newCrtstal = Instantiate(crystalToRespawn, player.transform.position, Quaternion.identity);
+
+                crystalList.Remove(crystalToRespawn);
+                newCrtstal.GetComponent<Crystal_Skill_Controller>()
+                    .SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed,
+                        FindClosestEnemy(newCrtstal.transform));
+                if (crystalList.Count <= 0)
+                {
+                    coolDown = multiStackCooldown;
+                    RefilCrystal();
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private void RefilCrystal()
+    {
+        int amountToAdd = amountOfStacks - crystalList.Count;
+
+        for (int i = 0; i < amountToAdd; i++)
+        {
+            crystalList.Add(crystalPrefab);
+        }
+    }
+
+    private void ResetAbility()
+    {
+        if (coolDownTimer > 0)
+        {
+            return;
+        }
+
+        coolDownTimer = multiStackCooldown;
+        RefilCrystal();
     }
 }
