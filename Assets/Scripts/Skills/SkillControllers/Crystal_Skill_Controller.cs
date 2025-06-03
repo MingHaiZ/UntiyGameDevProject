c970cda87196a73adcb13ab94a66053a35ee48e0
MingHaiZ <1163094232@qq.com>
2025-06-03 12:09:48 +0800

    <新增> 1.水晶爆炸效果 2.使用动画trigger控制

--- Assets/Scripts/Skills/SkillControllers/Crystal_Skill_Controller.cs
+++ Assets/Scripts/Skills/SkillControllers/Crystal_Skill_Controller.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 
 public class Crystal_Skill_Controller : MonoBehaviour
 {
+    private CircleCollider2D cd => GetComponent<CircleCollider2D>();
     private float crystalExistTimer;
+    private bool canExplode;
+    private bool canMoveToEnemy;
+    private float moveSpeed;
+    private Animator anim;
+    private bool canGrow;
+    private float growSpeed = 5;
 
-    public void SetupCrystal(float _crystalDuration)
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void SetupCrystal(float _crystalDuration, bool _canExplode, bool _canMove, float _moveSpeed)
     {
         crystalExistTimer = _crystalDuration;
+        canExplode = _canExplode;
+        canMoveToEnemy = _canMove;
+        moveSpeed = _moveSpeed;
     }
 
     private void Update()
@@ -17,9 +32,38 @@ public class Crystal_Skill_Controller : MonoBehaviour
         crystalExistTimer -= Time.deltaTime;
         if (crystalExistTimer < 0)
         {
+            FinishCrystal();
+        }
+
+        if (canGrow)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(3, 3), growSpeed * Time.deltaTime);
+        }
+    }
+
+    public void FinishCrystal()
+    {
+        if (canExplode)
+        {
+            canGrow = true;
+            anim.SetTrigger("Explode");
+        } else
+        {
             SelfDestroy();
         }
     }
 
-    public void SelfDestroy() => Destroy(gameObject);
+    private void SelfDestroy() => Destroy(gameObject);
+
+    private void AnimationExplodeEvent()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, cd.radius);
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                hit.GetComponent<Enemy>().Damage();
+            }
+        }
+    }
 }
