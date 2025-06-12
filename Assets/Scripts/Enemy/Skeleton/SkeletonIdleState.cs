e349223afd95eee478ea62f113da122ff33a8e12
MingHaiZ <1163094232@qq.com>
2025-06-12 16:27:07 +0800

    <修改> 1.添加了当玩家与目标距离最小距离过远时不播放音效的功能

--- Assets/Scripts/Enemy/Skeleton/SkeletonIdleState.cs
+++ Assets/Scripts/Enemy/Skeleton/SkeletonIdleState.cs
@@ -1,6 +1,7 @@
 public class SkeletonIdleState : SkeletonGroundedState
 {
-    public SkeletonIdleState(EnemyStateMachine stateMachine, Enemy enemyBase, string animBoolName, Enemy_Skeleton enemy) : base(stateMachine, enemyBase, animBoolName, enemy)
+    public SkeletonIdleState(EnemyStateMachine stateMachine, Enemy enemyBase, string animBoolName, Enemy_Skeleton enemy)
+        : base(stateMachine, enemyBase, animBoolName, enemy)
     {
     }
 
@@ -22,5 +23,6 @@ public class SkeletonIdleState : SkeletonGroundedState
     public override void Exit()
     {
         base.Exit();
+        AudioManager.instance.PlaySFX(24, enemy.transform);
     }
 }
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -5,6 +5,7 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
+    [SerializeField] private float sfxMinimumDistance;
     [SerializeField] private AudioSource[] sfx;
     [SerializeField] private AudioSource[] bgm;
 
@@ -36,8 +37,19 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    public void PlaySFX(int _sfxIndex)
+    public void PlaySFX(int _sfxIndex, Transform _source)
     {
+        if (sfx[_sfxIndex].isPlaying)
+        {
+            return;
+        }
+
+        if (_source != null &&
+            Vector2.Distance(PlayerManager.instance.transform.position, _source.position) > sfxMinimumDistance)
+        {
+            return;
+        }
+
         if (_sfxIndex < sfx.Length)
         {
             sfx[_sfxIndex].pitch = Random.Range(0.85f, 1.1f);
